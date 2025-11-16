7f57cbf010410e0a56a23bef7c6cce1ef4abc21d
dnatisdale <dant.grnt@gmail.com>
2025-11-16 07:21:40 -0800

    Update Icons.jsx

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -241,7 +241,24 @@ export const BookOpen = (props) => (
     <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
   </svg>
 );
+// Add this code to src/components/Icons.jsx
 
+export const ExternalLink = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+    />
+  </svg>
+);
 export const Qrcode = (props) => (
   <svg
     {...props}
